806fd340a6cf377cede9a1682e358204cee39294
mjewett2012 <michaeljewett@my.cwi.edu>
2024-10-23 09:24:15 -0600

    Added collision page

---
+++ Pages/Collision.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace total_test_1.Pages
+{
+    public class CollisionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
