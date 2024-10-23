75241da8d37c9186a63b35c7fba25ac183f4bcb7
Hunter Evans <moab@flibit.com>
2024-10-23 09:53:34 -0600

    Created Login Template

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace total_test_1.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
