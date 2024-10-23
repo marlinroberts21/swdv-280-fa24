0a6cfa7e4fb24214c2af1dec99811da4eae54a7d
tblacketter <tblacketter@gmail.com>
2024-10-23 09:32:35 -0600

    HTML changes to appointments page: add <br> tags to input fields

--- Pages/Appointments.cshtml.cs
+++ Pages/Appointments.cshtml.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace total_test_1.Pages
 {
     public class AppointmentsModel : PageModel
-    {
+    {   
         public void OnGet()
         {
         }
