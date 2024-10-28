03f034f294f5e226da37222c9f6e2ccfd4daec15
tblacketter <tblacketter@gmail.com>
2024-10-28 11:06:20 -0600

    Added time input and dropdown

--- Pages/Appointments.cshtml.cs
+++ Pages/Appointments.cshtml.cs
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using System.ComponentModel.DataAnnotations;
-=======
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
->>>>>>> main
+
 
 namespace total_test_1.Pages
 {
     public class AppointmentsModel : PageModel
-<<<<<<< HEAD
-    {   
-=======
     {
         private readonly ILogger<AppointmentsModel> _logger;
 
@@ -21,13 +15,8 @@ namespace total_test_1.Pages
             _logger = logger;
         }
 
->>>>>>> main
         public void OnGet()
         {
         }
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 }
--- Pages/Painting.cshtml.cs
+++ Pages/Painting.cshtml.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Mvc;
-=======
-using Microsoft.AspNetCore.Mvc;
->>>>>>> origin/main
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace total_test_1.Pages
@@ -18,15 +14,9 @@ namespace total_test_1.Pages
 
         public void OnGet()
         {
-<<<<<<< HEAD
-        }
-    }
-
-}
-=======
 
         }
     }
 }
 
->>>>>>> origin/main
+
