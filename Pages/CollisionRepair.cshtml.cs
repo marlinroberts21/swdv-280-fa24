9663f409dc36d4492f420fe47643c5d86eadf17f
rtaylor@cwiinst.com <rtaylor@cwiinst.com>
2024-10-28 10:10:59 -0600

    initial push to Ryan branch

---
+++ Pages/CollisionRepair.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace total_test_1.Pages
+{
+    public class CollisionRepairModel : PageModel
+    {
+        private readonly ILogger<CollisionRepairModel> _logger;
+
+        public CollisionRepairModel(ILogger<CollisionRepairModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ Pages/Painting.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace total_test_1.Pages
+{
+    public class PaintingModel : PageModel
+    {
+        private readonly ILogger<PaintingModel> _logger;
+
+        public PaintingModel(ILogger<PaintingModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
