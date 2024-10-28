5580e4fab26d156cf6a4d0a50350c57cd5078f8e
mjewett2012 <michaeljewett@my.cwi.edu>
2024-10-28 10:59:38 -0600

    Fixed filename and Nav
    Collision renamed to CollisionRepair, also fixed in Nav

--- Pages/CollisionRepair.cshtml.cs
+++ Pages/CollisionRepair.cshtml.cs
@@ -1,20 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace total_test_1.Pages
 {
-    public class CollisionRepairModel : PageModel
+    public class CollisionModel : PageModel
     {
-        private readonly ILogger<CollisionRepairModel> _logger;
-
-        public CollisionRepairModel(ILogger<CollisionRepairModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
         }
     }
-
 }
