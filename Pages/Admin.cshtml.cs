fd9f66503bd5e29ac743c324e84c1eefdeec8bdd
Hunter Evans <moab@flibit.com>
2024-11-10 19:08:01 -0700

    Removed unnecessary comments

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -12,35 +12,15 @@ namespace total_test_1.Pages
     public class AdminModel : PageModel
     {
         private readonly ILogger<AdminModel> _logger;
-        //private readonly ScheduleDbContext _context;
 
-        public AdminModel(ILogger<AdminModel> logger)/*(ScheduleDbContext context)*/
+
+        public AdminModel(ILogger<AdminModel> logger)
         {
             _logger = logger;
-            //_context = context;
         }
-        //public List<Customer> Customers { get; set; }
         public void OnGet()
         {
-            //this.Customers = _context.GetCustomers().ToList();
         }
-        //public void OnPost()
-        //{
-        //    Customers = _context.GetCustomers().ToList();
-        //}
-        
-        //[HttpGet]
-        //public async Task<ActionResult<List<Customer>>> GetCustomers()
-        //{
-        //    var result = await _context.Customers.ToListAsync();
-        //    return result;
-        //}
-        //public IActionResult Load()
-        //{
-        //    Customer customer = new Customer();
-        //    ViewBag
-        //}
 
-        //add a method that allows radio buttons to work as expected. Listviews don't play nice with radio buttons.
     }
 }
