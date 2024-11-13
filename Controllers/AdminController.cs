ee48a45d1b89cd78c518ea8b16dea393b518af5e
Hunter Evans <moab@flibit.com>
2024-11-13 10:53:44 -0700

    Changed AppointmentDisplay. Trying to display info we want.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -19,10 +19,31 @@ namespace total_test_1.Controllers
         }
         public IActionResult Schedule()
         {
-            AppointmentDisplay appointmentViewer = new AppointmentDisplay();
-            appointmentViewer.Customer = context.Customers;
-            appointmentViewer.Appointment = context.Appointments;
-            appointmentViewer.Time = context.Times;
+            //AppointmentDisplay appointmentViewer = new AppointmentDisplay();
+            //appointmentViewer.Customer = context.Customers;
+            //appointmentViewer.Appointment = context.Appointments;
+            //appointmentViewer.Time = context.Times;
+
+            List<AppointmentDisplay> appointmentViewer = new List<AppointmentDisplay>();
+            var customersAndAppointments = context.Appointments.Zip(context.Customers, (c, a) => new {Appointment = a, Customer = c});
+            int i = 0;
+            foreach(var ca in customersAndAppointments)
+            {
+                if (ca.Appointment.CustomerId == ca.Customer.CustomerId)
+                {
+                    appointmentViewer[i].FirstName = ca.Customer.Customer.FirstName;
+                    appointmentViewer[i].LastName = ca.Customer.Customer.LastName;
+                }
+                i++;
+            }
+            //foreach (Appointment appointment in context.Appointments)
+            //    foreach (Customer customer in context.Customers)
+                    
+            //if (appointment.CustomerId == customer.CustomerId)
+            //{
+            //            appointmentViewer[i].FirstName = customer.FirstName;
+            //            appointmentViewer
+            //}
 
             ViewBag.appointmentView = appointmentViewer;
             return View("../Admin");
--- Models/Admin/AppointmentDisplay.cs
+++ Models/Admin/AppointmentDisplay.cs
@@ -5,11 +5,14 @@ namespace total_test_1.Models.Admin
 {
     public class AppointmentDisplay
     {
-        public DbSet<Appointment> Appointment {  get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Time> Time { get; set; }
+        //public DbSet<Appointment> Appointment {  get; set; }
+        //public DbSet<Customer> Customer { get; set; }
+        //public DbSet<Time> Time { get; set; }
+        public string Time {  get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
         public AppointmentDisplay()
-        {    
+        {
         }
     }
 }
