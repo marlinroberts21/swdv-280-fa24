9612e65c84591a188abfd7ff99475ebcc145cf09
iridianarreola <iridian.arreola19@gmail.com>
2024-11-13 09:27:44 -0700

    Reviews Controller
    Added http post to add reviews

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using total_test_1.Models;
+using total_test_1.Models.Reviews;
 
 namespace total_test_1.Controllers
 {
     public class ReviewsController : Controller
-    {
+    { 
+        private ReviewsContext context;
+
+        public ReviewsController(ReviewsContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        // adds reviews
+        [HttpPost]
+        public IActionResult Add(string firstName, string lastName, string reviewText)
+        { 
+            if(ModelState.IsValid)
+            {
+                var reviewer = new Reviewer
+                {
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+
+                context.Reviewers.Add(reviewer);
+                context.SaveChanges();
+
+                var review = new Review
+                {
+                    ReviewerId = reviewer.ReviewerId,
+                    ReviewText = reviewText
+                };
+
+                context.Reviews.Add(review);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        
     }
 }
