95db9786fed0352fdafa63d80cad4cc2affd8ba8
Hunter Evans <moab@flibit.com>
2024-11-14 21:20:34 -0700

    Added table with selected day's appointments to admin page
    I also added authorization to all possible entry points of  the admin page.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using total_test_1.Models.Admin;
 using total_test_1.Models.Schedule;
 using total_test_1.Pages;
@@ -6,8 +7,10 @@ using total_test_1.Services;
 
 namespace total_test_1.Controllers
 {
-    public class AdminController : Controller
+	[Authorize]
+	public class AdminController : Controller
     {
+
         private ScheduleContext context;
         public AdminController(ScheduleContext ctx)
         {
@@ -17,56 +20,33 @@ namespace total_test_1.Controllers
         {
             return RedirectToAction("Schedule");
         }
-        public IActionResult Schedule()
+        public IActionResult Schedule(string? selectedDate)
         {
-            //AppointmentDisplay appointmentViewer = new AppointmentDisplay();
-            //appointmentViewer.Customer = context.Customers;
-            //appointmentViewer.Appointment = context.Appointments;
-            //appointmentViewer.Time = context.Times;
-
-            List<AppointmentDisplay> appointmentViewer = new List<AppointmentDisplay>();
-            int i = 0;
-			string? firstName = null;
-			string? lastName = null;
-			TimeOnly? theTime = null;
-			foreach (Customer customer in context.Customers)
+            DateOnly dateOnly;
+            if (selectedDate == null)
             {
-				int j = 0;
-                foreach(Appointment appointment in context.Appointments)
-                {
-                    foreach (Time time in context.Times)
-                    {
-
+                dateOnly = DateOnly.Parse(DateTime.Today.ToString("MM/dd/yyyy"));
+                ViewBag.s = DateTime.Today.ToString("yyyy-MM-dd");
+			}
+            else 
+            {
+				dateOnly = DateOnly.Parse(selectedDate);
+                ViewBag.s = selectedDate;
+			}
+			var appointmentViewer = (
+                                            from customer in context.Customers
+                                            join appointment in context.Appointments
+                                                on customer.CustomerId equals appointment.CustomerId
+                                            join time in context.Times
+                                                on appointment.TimeId equals time.TimeId
+                                            where appointment.Date == dateOnly
+                                            select new AppointmentDisplay(time.Time1, customer.FirstName, customer.LastName)
+                                        );
+           
+                ViewBag.appointmentViewer = appointmentViewer;
                 
-                    if (appointment.CustomerId == customer.CustomerId && appointment.TimeId == time.TimeId)
-                    {
-                        firstName = customer.FirstName;
-                        lastName = customer.LastName;
-                        theTime = time.Time1;
-                    }
-                    int k = 0;
-                                            
-                    k++;
-                    }
-                j++;
-                }
-            i++;
-                    appointmentViewer.Add(new AppointmentDisplay(theTime, firstName, lastName));
-                firstName = null;
-                lastName = null;
-                theTime = null;
-            }
-            //foreach (Appointment appointment in context.Appointments)
-            //    foreach (Customer customer in context.Customers)
-                    
-            //if (appointment.CustomerId == customer.CustomerId)
-            //{
-            //            appointmentViewer[i].FirstName = customer.FirstName;
-            //            appointmentViewer
-            //}
-
-            ViewBag.appointmentViewer = appointmentViewer;
-            return View("../Admin");
+			
+			return View("../Admin");
         }
     }
 }
