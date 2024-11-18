086dc074c53b1a5268af70fe22b79573529dd386
gagetranter <130384485+GageSt@users.noreply.github.com>
2024-11-18 10:29:22 -0700

    Reviews Page Scroll Box
    Changed Height Limit of Reviews window.

--- Pages/Reviews.cshtml.cs
+++ Pages/Reviews.cshtml.cs
@@ -5,8 +5,9 @@ namespace total_test_1.Pages
 {
     public class ReviewsModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string? rate)
         {
+
         }
     }
 }
